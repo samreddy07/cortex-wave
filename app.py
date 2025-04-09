7706b72a8509394a7827e69fa6c0b2c7d65096d0
SAMARASIMHA REDDY <49056589+samreddy07@users.noreply.github.com>
2025-04-09 09:52:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -116,14 +116,16 @@ with st.sidebar:
            st.session_state.pdf_processed = True
            st.success("✅ PDF processed and added to FAISS!")
    st.markdown('📖 Learn how to build this app in this blog!')
+def clear_chat_history():
+    st.session_state.faiss_store.clear()
+    st.session_state.clear()
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
    st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
    # if st.sidebar.button('Clear Chat History'):
    #     st.session_state.faiss_store.clear()
    #     st.session_state.clear()
    #     st.rerun()
    #     st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
-def clear_chat_history():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
 # === Response Generation ===
 def generate_response(user_input):
    query_embedding = get_embedding(user_input)
